5b531ebef02d31dec116ee5e6e2d1bff6b7e4501
ujjwalshrivastava786 <sricasujjwal@gmail.com>
2020-10-02 12:49:14 +0530

    MY FIRST COMMIT

---
+++ Starting.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Starting{
+public static void main(String [] args)
+{
+int x;
+float y;
+char z;
+String a;
+Scanner obj = new Scanner(System.in);
+System.out.println("enter a no.= ");
+x=obj.nextInt();
+System.out.println("You have entered "+x+" ");
+System.out.println("Enter a num=");
+y=obj.nextFloat();
+System.out.println("You have entered "+y+" ");
+System.out.println("enter a num=");
+z=obj.next().charAt(0);
+System.out.println("you have enterd "+z+" ");
+}
+}
